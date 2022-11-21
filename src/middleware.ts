3084a1252287c59007e58048c50baa5f2227faf7
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2022-11-21 18:39:37 -0300

    fix: fixing cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
   if (req.nextUrl.pathname.startsWith('/s')) {
+    // cors
     console.log('path??', req.nextUrl.pathname);
 
     const slug = req.nextUrl.pathname.split('/').pop();
@@ -17,5 +18,11 @@ export async function middleware(req: NextRequest, event: NextFetchEvent) {
     }
 
     console.log('slug??', slug);
+  }
+
+  if (req.nextUrl.pathname.startsWith('/api/shorten')) {
+    const domain = process.env.NEXT_PUBLIC_DOMAIN || 'http://localhost:3000';
+    // allow cors
+    req.headers.set('Access-Control-Allow-Origin', domain);
   }
 }
