8926c86c4f714abc9fb0f03b0def91752038f858
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-11-01 00:05:02 -0300

    feat: add loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,47 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import { FormEvent, useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import boltIcon from '../assets/bolt.svg';
-import api from '../lib/axios';
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import boltIcon from "../assets/bolt.svg";
+import api from "../lib/axios";
 
 export default function Home() {
-  const [url, setUrl] = useState('');
-  const [slug, setSlug] = useState('');
+  const [url, setUrl] = useState("");
+  const [slug, setSlug] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const shortenUrl = async (e: FormEvent) => {
     e.preventDefault();
     if (!url) {
-      toast.error('Please enter a URL', {
+      toast.error("Please enter a URL", {
         autoClose: 2000,
       });
       return;
     }
-    const res = await api.post('/api/shorten', {
-      url,
-      slug,
-    });
 
-    const { data } = res;
-    // copy to clipboard
-    navigator.clipboard.writeText(data.link);
-    toast.success('Copied to clipboard!', {
-      autoClose: 3000,
-    });
+    setLoading(true);
+    const res = api
+      .post("/api/shorten", {
+        url,
+        slug,
+      })
+      .then((res) => {
+        const { data } = res;
+        // copy to clipboard
+        navigator.clipboard.writeText(data.link);
+        toast.success("Copied to clipboard!", {
+          autoClose: 3000,
+        });
+      })
+      .catch((e) => {
+        toast.error("An error occurred, please try again later", {
+          autoClose: 3000,
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
   return (
@@ -36,83 +49,82 @@ export default function Home() {
       <Head>
         <title>Raijin</title>
         <meta
-          name='description'
-          content='Raijin is a url shortener! made with nextjs'
+          name="description"
+          content="Raijin is a url shortener! made with nextjs"
         />
-        <meta
-          name='viewport'
-          content='initial-scale=1.0, width=device-width'
-        />
-        <meta
-          name='author'
-          content='@rafaelsilva81'
-        />
-        <link
-          rel='icon'
-          href='/favicon.ico'
-        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="author" content="@rafaelsilva81" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='h-screen w-screen flex flex-1 justify-center items-center bg-gradient-to-br from-sky-800 to-violet-900'>
-        <div className='flex flex-col p-4 gap-2 md:p-0'>
-          <div className='flex items-stretch'>
-            <h1 className='text-6xl font-bold text-white'> Raijin </h1>
+      <main className="h-screen w-screen flex flex-1 justify-center items-center bg-gradient-to-br from-sky-800 to-violet-900">
+        <div className="flex flex-col p-4 gap-2 md:p-0">
+          <div className="flex items-stretch">
+            <h1 className="text-6xl font-bold text-white"> Raijin </h1>
             <Image
               src={boltIcon}
-              alt='Raijin'
-              className='h-16 w-16'
-              style={{ transform: 'scale(-1,1)' }}
+              alt="Raijin"
+              className="h-16 w-16"
+              style={{ transform: "scale(-1,1)" }}
             />
           </div>
-          <p className='text-xl text-white'>
-            Simple URL Shortener made by {''}
-            <Link
-              href='https://rafaeldev.me'
-              className='text-sky-400'>
+          <p className="text-xl text-white">
+            Simple URL Shortener made by {""}
+            <Link href="https://rafaeldev.me" className="text-sky-400">
               @rafaelsilva81
             </Link>
           </p>
 
-          <form
-            onSubmit={shortenUrl}
-            className='flex flex-col gap-2 mt-4'>
+          <form onSubmit={shortenUrl} className="flex flex-col gap-2 mt-4">
             <input
-              type='text'
-              className='rounded-md p-2 h-12'
-              placeholder='Paste your URL here'
-              name='url'
+              type="text"
+              className="rounded-md p-2 h-12"
+              placeholder="Paste your URL here"
+              name="url"
               defaultValue={url}
               onChange={(e) => setUrl(e.target.value)}
             />
-            <div className='flex flex-1 items-center gap-1'>
-              <span className='text-white text-lg font-bold'>raijin.tk/s/</span>
+            <div className="flex flex-1 items-center gap-1">
+              <span className="text-white text-lg font-bold">raijin.tk/s/</span>
               <input
-                type='text'
-                className='rounded-md p-2 h-12 flex-1'
-                placeholder='Custom URL (optional)'
-                name='slug'
+                type="text"
+                className="rounded-md p-2 h-12 flex-1"
+                placeholder="Custom URL (optional)"
+                name="slug"
                 defaultValue={slug}
                 onChange={(e) => setSlug(e.target.value)}
               />
             </div>
 
-            <button
-              type='submit'
-              className='bg-sky-600 rounded-md text-white p-2 h-12 font-bold uppercase hover:bg-sky-500 transition ease-in-out'>
-              Shorten!
-            </button>
+            {loading ? (
+              <button
+                type="submit"
+                disabled
+                className="bg-gray-500 rounded-md text-white p-2 h-12 font-bold uppercase transition ease-in-out"
+              >
+                Loading...
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="bg-sky-600 rounded-md text-white p-2 h-12 font-bold uppercase hover:bg-sky-500 transition ease-in-out"
+              >
+                Shorten!
+              </button>
+            )}
           </form>
 
-          <div className='flex flex-col mt-10'>
+          <div className="flex flex-col mt-10">
             <a
-              href='https://github.com/rafaelsilva81/raijin'
-              className='text-white text-lg font-bold'>
+              href="https://github.com/rafaelsilva81/raijin"
+              className="text-white text-lg font-bold"
+            >
               See the code on Github
             </a>
           </div>
         </div>
         <ToastContainer
-          position='bottom-center'
+          position="bottom-center"
           hideProgressBar={false}
           newestOnTop={false}
           closeOnClick
