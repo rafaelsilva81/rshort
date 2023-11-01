cee8da5e39d73e613ad487b2ba5a0853f6b12620
Rafael Galdino da Silva <rafaelgaldinosilva81@gmail.com>
2023-11-01 00:14:18 -0300

    feat: nevermind hello again npm for some reason

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import 'react-toastify/dist/ReactToastify.css';
+import type { AppProps } from "next/app";
+import "react-toastify/dist/ReactToastify.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
